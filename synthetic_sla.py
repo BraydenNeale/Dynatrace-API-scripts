41cb85c2edc2ca1419ba8bb119d67a305af133b4
Brayden Neale <brayden.neale@dynatrace.com>
2019-11-04 17:05:27 +1100

    refactor api call into function

--- synthetic_sla.py
+++ synthetic_sla.py
@@ -8,25 +8,29 @@ import csv
 import numpy as np
 import requests
 
-if __name__ == '__main__':
-	with open('config.json') as fp:
-		config = json.load(fp)
-
-	dt_url = config.get('url')
-	dt_token = config.get('token')
-	timeseries_id = 'com.dynatrace.builtin%3Asyntheticmonitor.availability.percent'
-	relative_time = 'day' # hour, day, month, week
-
-	request_url = f'{dt_url}/api/v1/timeseries/{timeseries_id}?includeData=true&relativeTime={relative_time}'
+def dynatrace_api_request(url, token, timeseries_id, relative_time='day'):
+	request_url = f'{url}/api/v1/timeseries/{timeseries_id}?includeData=true&relativeTime={relative_time}'
 	headers = { 
 		'Accept': 'application/json', 
-		'Authorization': f'Api-Token {dt_token}'
+		'Authorization': f'Api-Token {token}'
 	}
 
 	response = requests.get(request_url, headers=headers)
 	response.raise_for_status()
 
-	dt_json = response.json()
+	return response.json()
+
+if __name__ == '__main__':
+	# read config
+	with open('config.json') as fp:
+		config = json.load(fp)
+
+	url = config.get('url')
+	token = config.get('token')
+	timeseries_id = 'com.dynatrace.builtin%3Asyntheticmonitor.availability.percent'
+	relative_time = 'day' # hour, day, month, week
+
+	dt_json = dynatrace_api_request(url, token, timeseries_id, relative_time)
 	data_result = dt_json['dataResult']
 	data = data_result['dataPoints']
 
